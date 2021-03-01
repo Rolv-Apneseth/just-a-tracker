fb4e498f5bf6548196d25b54eb3523a6f810a5b7
Rolv Apneseth <rolv.apneseth@gmail.com>
2021-03-01 17:49:15 +0000

    Added some more html templates

--- just-a-tracker/website/views.py
+++ just-a-tracker/website/views.py
@@ -7,3 +7,23 @@ views = Blueprint("views", __name__)
 @views.route("/")
 def homepage():
     return render_template("home.html")
+
+
+@views.route("/account")
+def account():
+    return render_template("account.html")
+
+
+@views.route("/create-workspace")
+def create_workspace():
+    return render_template("create_workspace.html")
+
+
+@views.route("/workspace-hub")
+def workspace_hub():
+    return render_template("hub.html")
+
+
+@views.route("/workspace")
+def workspace():
+    return render_template("workspace.html", project_id=1234563)
