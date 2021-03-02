f270d6778323776c04e2373d992843377da09899
Rolv Apneseth <rolv.apneseth@gmail.com>
2021-03-02 17:34:03 +0000

    Added more conditions for error flash messages to appear

--- just-a-tracker/website/auth.py
+++ just-a-tracker/website/auth.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template, request, flash
+import re
 
 
 auth = Blueprint("auth", __name__)
+
+
+# CONSTANTS
+RE_USERNAME = r"^[\w-]{6,}$"
+RE_PASSWORD = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\w\W]{8,}$"
 
 
 @auth.route("/login", methods=["GET", "POST"])
@@ -28,7 +34,18 @@ def sign_up():
                 "Your passwords did not match. Please make "
                 "sure you type the same password in both fields.",
             )
-        else:
-            pass
+
+        if not re.match(RE_PASSWORD, password):
+            flash(
+                "Your password must be at least 8 characters long and"
+                " contain: 1 uppercase letter, 1 lowercase letter and"
+                " 1 number"
+            )
+
+        if not re.match(RE_USERNAME, username):
+            flash(
+                "Username must be 6 characters or longer, and can only "
+                "include letters, numbers and the symbols '_' and '-'"
+            )
 
     return render_template("sign_up.html")
