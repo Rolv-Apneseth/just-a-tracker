3b0c33debea18c4d764ab4ae6e574fd233d09564
Rolv Apneseth <rolv.apneseth@gmail.com>
2021-03-02 15:01:28 +0000

    Added post method to sign up route and wrote some basic code for handling form submission

--- just-a-tracker/website/auth.py
+++ just-a-tracker/website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
@@ -14,6 +14,21 @@ def logout():
     return "<p>Logout</p>"
 
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        form = request.form
+        email = form.get("email")
+        username = form.get("username")
+        password = form.get("password")
+        password_confirm = form.get("password-confirm")
+
+        if password != password_confirm:
+            flash(
+                "Your passwords did not match. Please make "
+                "sure you type the same password in both fields.",
+            )
+        else:
+            pass
+
     return render_template("sign_up.html")
