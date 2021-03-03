213842d7f560238547ce013f33a85f66cc4bedf3
Rolv Apneseth <rolv.apneseth@gmail.com>
2021-03-03 13:19:14 +0000

    Added code for creating  and configuring database

--- just-a-tracker/website/__init__.py
+++ just-a-tracker/website/__init__.py
@@ -1,10 +1,16 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.environ["JUST_A_TRACKER_KEY"]
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -12,4 +18,20 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
+    from .models import (
+        users_workspaces,
+        bugs_workspaces,
+        Workspace,
+        User,
+        Bug,
+    )
+
+    create_database(app)
+
     return app
+
+
+def create_database(app):
+    if not os.path.exists(os.path.join("website", DB_NAME)):
+        db.create_all(app=app)
+        print("Database created")
