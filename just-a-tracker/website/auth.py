8c95bb99223c68383cdfabc0f10a97c40d24dee3
Rolv Apneseth <rolv.apneseth@gmail.com>
2021-03-04 17:14:15 +0000

    Added conditionals to validate_sign_up_info for checking if an account with those unique credentials already exists. Also added function to provide login functionality.

--- just-a-tracker/website/auth.py
+++ just-a-tracker/website/auth.py
@@ -26,10 +26,30 @@ USER_FORMAT = (
     "Username must be 6 characters or longer, and can only "
     "include letters, numbers and the symbols '_' and '-'"
 )
+USER_EXISTS = "Sorry, an account with that username already exists."
+EMAIL_EXISTS = "Sorry, an account with that email address already exists"
 
 
 # HELPERS
-def validate_info(form):
+def get_user_by_username(username):
+    return User.query.filter_by(username=username).first()
+
+
+def get_user_by_email(email):
+    return User.query.filter_by(email=email).first()
+
+
+def find_user(username):
+    """Tries to get the user by username or email address and returns the result."""
+
+    user = get_user_by_username(username)
+    if not user:
+        user = get_user_by_email(username)
+
+    return user
+
+
+def validate_sign_up_info(form):
     email = form.get("email")
     username = form.get("username")
     password = form.get("password")
@@ -43,6 +63,14 @@ def validate_info(form):
             "password": generate_password_hash(password, method="sha256"),
         },
     )
+
+    if get_user_by_username(username):
+        flash(USER_EXISTS)
+        result = (False, {})
+
+    if get_user_by_email(email):
+        flash(EMAIL_EXISTS)
+        result = (False, {})
 
     if password != password_confirm:
         flash(PASS_MATCH)
@@ -59,9 +87,34 @@ def validate_info(form):
     return result
 
 
+def validate_login_info(form):
+    username = form.get("username")
+    password = form.get("password")
+
+    result = (True, username)
+
+    user = find_user(username)
+
+    # Check password
+    if user:
+        if check_password_hash(user.password, password):
+            pass  # login
+        else:
+            flash("Password was incorrect. Please try again.")
+            result = (False, None)
+    else:
+        flash("No account matches that username or email")
+        result = (False, None)
+
+    return result
+
+
 # ROUTES
 @auth.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        validated = validate_login_info(request.form)
+
     return render_template("login.html")
 
 
@@ -73,7 +126,7 @@ def logout():
 @auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
     if request.method == "POST":
-        validated = validate_info(request.form)
+        validated = validate_sign_up_info(request.form)
 
         if validated[0]:
             new_user = User(**validated[1])
