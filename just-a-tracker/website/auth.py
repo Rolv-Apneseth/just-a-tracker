6273903bb3b44c3e602f8e16e181ea47e1cb5bcd
Rolv Apneseth <rolv.apneseth@gmail.com>
2021-03-04 14:15:43 +0000

    Moved code for validating account generation info to a helper function and implemented code to add a new account to the database if information was valid

--- just-a-tracker/website/auth.py
+++ just-a-tracker/website/auth.py
@@ -1,5 +1,9 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 import re
+
+from . import db
+from .models import User
 
 
 auth = Blueprint("auth", __name__)
@@ -9,7 +13,53 @@ auth = Blueprint("auth", __name__)
 RE_USERNAME = r"^[\w-]{6,}$"
 RE_PASSWORD = r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\w\W]{8,}$"
 
+PASS_MATCH = (
+    "Your passwords did not match. Please make "
+    "sure you type the same password in both fields."
+)
+PASS_FORMAT = (
+    "Your password must be at least 8 characters long and"
+    " contain: 1 uppercase letter, 1 lowercase letter and"
+    " 1 number"
+)
+USER_FORMAT = (
+    "Username must be 6 characters or longer, and can only "
+    "include letters, numbers and the symbols '_' and '-'"
+)
 
+
+# HELPERS
+def validate_info(form):
+    email = form.get("email")
+    username = form.get("username")
+    password = form.get("password")
+    password_confirm = form.get("password-confirm")
+
+    result = (
+        True,
+        {
+            "email": email,
+            "username": username,
+            "password": generate_password_hash(password, method="sha256"),
+        },
+    )
+
+    if password != password_confirm:
+        flash(PASS_MATCH)
+        result = (False, {})
+
+    if not re.match(RE_PASSWORD, password):
+        flash(PASS_FORMAT)
+        result = (False, {})
+
+    if not re.match(RE_USERNAME, username):
+        flash(USER_FORMAT)
+        result = (False, {})
+
+    return result
+
+
+# ROUTES
 @auth.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
@@ -23,29 +73,13 @@ def logout():
 @auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
     if request.method == "POST":
-        form = request.form
-        email = form.get("email")
-        username = form.get("username")
-        password = form.get("password")
-        password_confirm = form.get("password-confirm")
+        validated = validate_info(request.form)
 
-        if password != password_confirm:
-            flash(
-                "Your passwords did not match. Please make "
-                "sure you type the same password in both fields.",
-            )
+        if validated[0]:
+            new_user = User(**validated[1])
+            db.session.add(new_user)
+            db.session.commit()
 
-        if not re.match(RE_PASSWORD, password):
-            flash(
-                "Your password must be at least 8 characters long and"
-                " contain: 1 uppercase letter, 1 lowercase letter and"
-                " 1 number"
-            )
-
-        if not re.match(RE_USERNAME, username):
-            flash(
-                "Username must be 6 characters or longer, and can only "
-                "include letters, numbers and the symbols '_' and '-'"
-            )
+            return redirect(url_for("views.account"))
 
     return render_template("sign_up.html")
