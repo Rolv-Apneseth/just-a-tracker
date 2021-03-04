c5810c00ed4fd7e9bf82d6f86e96575738916026
Rolv Apneseth <rolv.apneseth@gmail.com>
2021-03-04 12:44:26 +0000

    Small changes to models.py, removed unnecessary table

--- just-a-tracker/website/models.py
+++ just-a-tracker/website/models.py
@@ -5,27 +5,24 @@ from sqlalchemy.sql import func
 
 users_workspaces = db.Table(
     "users_wokspaces",
-    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
-    db.Column("workspace_id", db.Integer, db.ForeignKey("workspace.id")),
-)
-
-bugs_workspaces = db.Table(
-    "bugs_workspaces",
-    db.Column("bug_id", db.Integer, db.ForeignKey("bug.id")),
-    db.Column("workspace_id", db.Integer, db.ForeignKey("workspace.id")),
+    db.Column("user_id", db.Integer, db.ForeignKey("user.user_id")),
+    db.Column("workspace_id", db.Integer, db.ForeignKey("workspace.workspace_id")),
 )
 
 
 class Workspace(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    project_name = db.Column(db.String(128), unique=True)
+    workspace_id = db.Column(db.Integer, primary_key=True)
+    project_name = db.Column(db.String(128))
+
+    bugs = db.relstionship("Bug")
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(320), unique=True)
     password = db.Column(db.String(128))
     username = db.Column(db.String(50))
+
     bugs = db.relationship("Bug")
     workspaces = db.relationship(
         "Workspace",
@@ -35,13 +32,10 @@ class User(db.Model, UserMixin):
 
 
 class Bug(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    bug_id = db.Column(db.Integer, primary_key=True)
     bug_title = db.Column(db.String(128))
     bug_description = db.Column(db.String(512))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
-    author_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-    workspaces = db.relationship(
-        "Workspace",
-        secondary=users_workspaces,
-        backref=db.backref("users", lazy="dynamic"),
-    )
+
+    author_id = db.Column(db.Integer, db.ForeignKey("user.user_id"))
+    workspaces = db.Column(db.Integer, db.ForeignKey("workspace.workspace_id"))
