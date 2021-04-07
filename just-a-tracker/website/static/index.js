c999c17cbdd42ced25b27b7e392a59711cba4464
Rolv-Apneseth <rolv.apneseth@gmail.com>
2021-04-07 21:40:50 +0100

    Added a check before deleting a workspace in index.js

--- just-a-tracker/website/static/index.js
+++ just-a-tracker/website/static/index.js
@@ -1,8 +1,15 @@
-function deleteWorkspace(workspaceID) {
-  fetch("/delete-workspace", {
-    method: "POST",
-    body: JSON.stringify({ workspaceID: workspaceID }),
-  }).then((_res) => {
-    window.location.href = "/workspace-hub";
-  });
+function deleteWorkspace(workspaceID, projectName) {
+  console.log(projectName);
+  var deleteWorkspaceMessage = `The workspace '${projectName.toString()}' and any data associated with it will be permanently deleted.
+  
+  If you really sure you wish to delete this workspace, click 'OK'.`;
+
+  if (confirm(deleteWorkspaceMessage)) {
+    fetch("/delete-workspace", {
+      method: "POST",
+      body: JSON.stringify({ workspaceID: workspaceID }),
+    }).then((_res) => {
+      window.location.href = "/workspace-hub";
+    });
+  }
 }
