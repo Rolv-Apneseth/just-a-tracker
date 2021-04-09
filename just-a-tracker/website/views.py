5425fa1355a06fe5fd2cb2b936c58bef9a6bd3d9
Rolv-Apneseth <rolv.apneseth@gmail.com>
2021-04-09 14:40:28 +0100

    Moved code for adding a bug to the database from a post request on the workspace page to a helper function, so it will be clearer to see the other options for post requests on the workspace page

--- just-a-tracker/website/views.py
+++ just-a-tracker/website/views.py
@@ -5,9 +5,29 @@ import json
 from . import db
 from .models import Workspace, Bug
 
+
 views = Blueprint("views", __name__)
 
 
+# HELPER FUNCTIONS
+def add_bug_to_workspace(db, current_user, data, workspace_id):
+    """Adds a bug object connected to the given workspace to the database."""
+
+    info = {
+        "bug_title": data.get("bug-title"),
+        "bug_description": data.get("bug-description"),
+        "author_id": current_user.user_id,
+        "author_username": current_user.username,
+        "workspace_id": workspace_id,
+    }
+
+    if info["bug_title"] and info["bug_description"]:
+        new_bug = Bug(**info)
+        db.session.add(new_bug)
+        db.session.commit()
+
+
+# ROUTES
 @views.route("/")
 def homepage():
     return render_template("home.html", user=current_user)
@@ -45,6 +65,7 @@ def workspace_hub():
 
 
 @views.route("/delete-workspace", methods=["POST"])
+@login_required
 def delete_workspace():
     workspace_id = json.loads(request.data)["workspaceID"]
     workspace = Workspace.query.get(workspace_id)
@@ -62,18 +83,12 @@ def workspace(workspace_id):
     workspace_object = Workspace.query.filter_by(workspace_id=workspace_id).first()
 
     if request.method == "POST":
-        info = {
-            "bug_title": request.form.get("bug-title"),
-            "bug_description": request.form.get("bug-description"),
-            "author_id": current_user.user_id,
-            "author_username": current_user.username,
-            "workspace_id": workspace_object.workspace_id,
-        }
+        data = request.form
 
-        if info["bug_title"] and info["bug_description"]:
-            new_bug = Bug(**info)
-            db.session.add(new_bug)
-            db.session.commit()
+        if data.get("collaborator"):
+            pass
+        else:
+            add_bug_to_workspace(db, current_user, data, workspace_id)
 
     if workspace_object and current_user in workspace_object.users:
         return render_template(
