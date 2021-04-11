89d0f663a8d22ff61d5fdaa6bf7a0bbf6e6a072a
Rolv-Apneseth <rolv.apneseth@gmail.com>
2021-04-11 15:31:58 +0100

    Made function used for sending post request to remove-user route in the backend

--- just-a-tracker/website/static/index.js
+++ just-a-tracker/website/static/index.js
@@ -1,5 +1,5 @@
 function deleteWorkspace(workspaceID, projectName) {
-  var deleteWorkspaceMessage = `The workspace '${projectName.toString()}' and any data associated with it will be permanently deleted.
+  const deleteWorkspaceMessage = `The workspace '${projectName.toString()}' and any data associated with it will be permanently deleted.
   
   If you really sure you wish to delete this workspace, click 'OK'.`;
 
@@ -9,6 +9,25 @@ function deleteWorkspace(workspaceID, projectName) {
       body: JSON.stringify({ workspaceID: workspaceID }),
     }).then((_res) => {
       window.location.href = "/workspace-hub";
+    });
+  }
+}
+
+function removeUser(workspaceID, projectName, userID, username, leaving) {
+  let message = "";
+  if (leaving == "true") {
+    message = `Click ok if you are sure you wish to leave '${projectName}'.`;
+  } else {
+    message = `Click ok if you are sure you wish to remove '${username}' from the 
+    workspace '${projectName}'`;
+  }
+
+  if (confirm(message)) {
+    fetch("/remove-user", {
+      method: "POST",
+      body: JSON.stringify({ workspaceID: workspaceID, userID: userID }),
+    }).then((_res) => {
+      window.location.href = `/workspace/${workspaceID}`;
     });
   }
 }
