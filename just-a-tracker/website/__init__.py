f9493b585db3310970c41c043aa9f09805c3966d
Rolv-Apneseth <rolv.apneseth@gmail.com>
2021-05-29 13:36:46 +0100

    Implemented use of environment variable which states whether environment is for production or development. Different config options for the flask app are set and the corresponding environment variables have been set up on the heroku app.

--- just-a-tracker/website/__init__.py
+++ just-a-tracker/website/__init__.py
@@ -3,16 +3,28 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-db = SQLAlchemy()
+
+# CONSTANTS
 DB_NAME = "database.db"
+LOCAL_DATABASE_PATH = os.path.join("website", DB_NAME)
+
+
+db = SQLAlchemy()
 
 
 def create_app():
     # Set up app
     app = Flask(__name__)
     app.config["SECRET_KEY"] = os.environ["JUST_A_TRACKER_KEY"]
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    ENV = os.environ["JUST_A_TRACKER_ENV"]
+    if ENV == "dev":
+        app.debug = True
+        app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    elif ENV == "prod":
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
+
     db.init_app(app)
 
     # Login Manager
@@ -32,20 +44,21 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
-    # Create database
-    from .models import (
-        users_workspaces,
-        Workspace,
-        User,
-        Bug,
-    )
+    # Create local database if it does not yet exist
+    if ENV == "dev":
+        from .models import (
+            users_workspaces,
+            Workspace,
+            User,
+            Bug,
+        )
 
-    create_database(app)
+        create_database(app)
 
     return app
 
 
 def create_database(app):
-    if not os.path.exists(os.path.join("website", DB_NAME)):
+    if not os.path.exists(LOCAL_DATABASE_PATH):
         db.create_all(app=app)
-        print("Database created")
+        print("Local database created")
